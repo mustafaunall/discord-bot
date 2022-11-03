0b6cbc3d78d81223727671570e5c7e87c3f8a006
Mustafa Ünal <mustafaunaldev@gmail.com>
2022-11-04 02:23:31 +0300

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,29 +1,27 @@
-const { MessageEmbed } = require('discord.js')
-const config = require('../config.json')
-const localization = require(`../localization/${config.localization_file}`)
+const { MessageEmbed } = require("discord.js");
+const config = require("../config.json");
+const localization = require(`../localization/${config.localization_file}`);
 
 module.exports.info = {
-    title: localization.commands.ping.title,
-    name: localization.commands.ping.name,
-    color: localization.commands.ping.color,
-    desc: localization.commands.ping.desc,
-}
+  title: localization.commands.ping.title,
+  name: localization.commands.ping.name,
+  color: localization.commands.ping.color,
+  desc: localization.commands.ping.desc,
+};
 
 module.exports.execute = async (client, message) => {
-    const embed = new MessageEmbed()
-        .setTitle(this.info.title)
-        .setColor(this.info.color)
-    
-    await client.commands.forEach(command => {
-        if (command.info.desc)
-            embed.addField(`${client.config.prefix}${command.info.name}`, command.info.desc)
-    })
+  const embed = new MessageEmbed()
+    .setTitle(this.info.title)
+    .setColor(this.info.color);
 
-    embed.setFooter({
-        text: message.member.displayName,
-        iconURL: message.author.displayAvatarURL({ dynamic: true }),
-    })
-    .setTimestamp()
+  embed.addField(this.info.title);
 
-    message.channel.send({ embeds: [ embed ] })
-}
+  embed
+    .setFooter({
+      text: message.member.displayName,
+      iconURL: message.author.displayAvatarURL({ dynamic: true }),
+    })
+    .setTimestamp();
+
+  message.channel.send({ embeds: [embed] });
+};
