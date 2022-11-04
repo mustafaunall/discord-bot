5f15eb89f031077b91cc27faaabc550eee911f7d
Mustafa Ünal <mustafaunaldev@gmail.com>
2022-11-05 01:02:26 +0300

    .

--- commands/ping.js
+++ commands/ping.js
@@ -10,18 +10,6 @@ module.exports.info = {
 };
 
 module.exports.execute = async (client, message) => {
-  const embed = new MessageEmbed()
-    .setTitle(this.info.title)
-    .setColor(this.info.color);
 
-  embed.addField(this.info.title);
-
-  embed
-    .setFooter({
-      text: message.member.displayName,
-      iconURL: message.author.displayAvatarURL({ dynamic: true }),
-    })
-    .setTimestamp();
-
-  message.channel.send({ embeds: [embed] });
+  message.channel.send(this.info.title);
 };
